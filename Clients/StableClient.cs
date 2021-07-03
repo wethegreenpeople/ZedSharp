87aad335b7bc6ce2339028996e7443d5db6a5fe3
John <wethegreenpeople@gmail.com>
2021-07-03 15:39:44 -0700

    Add stable model

--- Clients/StableClient.cs
+++ Clients/StableClient.cs
@@ -11,13 +11,21 @@ namespace ZedSharp.Clients
 {
     public class StableClient
     {
-        public async Task<List<Horse>> GetStableAsync(string address)
+        public async Task<List<Horse>> GetStableHorsesAsync(string address)
         {
             var request = new RestRequest("horses/get_user_horses", Method.GET);
             request.AddQueryParameter("public_address", address);
 
             var response = await RestHelper.Client.ExecuteGetAsync<List<Horse>>(request);
             return response.Data.OrderByDescending(s => s.Id).ToList();
+        }
+
+        public async Task<Stable> GetStableAsync(string stableName)
+        {
+            var request = new RestRequest($"users/stable/{stableName}", Method.GET);
+
+            var response = await RestHelper.Client.ExecuteGetAsync<Stable>(request);
+            return response.Data;
         }
     }
 }
---
+++ Models/Stable.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace ZedSharp.Models
+{
+    public class Career
+    {
+        [JsonPropertyName("first")]
+        public int First { get; set; }
+
+        [JsonPropertyName("second")]
+        public int Second { get; set; }
+
+        [JsonPropertyName("third")]
+        public int Third { get; set; }
+    }
+
+    public class StableAvatarUrls
+    {
+        [JsonPropertyName("original")]
+        public string Original { get; set; }
+
+        [JsonPropertyName("thumb")]
+        public string Thumb { get; set; }
+    }
+
+    public class StableStats
+    {
+        [JsonPropertyName("account_type")]
+        public string AccountType { get; set; }
+
+        [JsonPropertyName("career")]
+        public Career Career { get; set; }
+
+        [JsonPropertyName("discord_id")]
+        public object DiscordId { get; set; }
+
+        [JsonPropertyName("is_email_verified")]
+        public bool IsEmailVerified { get; set; }
+
+        [JsonPropertyName("public_address")]
+        public string PublicAddress { get; set; }
+
+        [JsonPropertyName("stable_avatar_urls")]
+        public StableAvatarUrls StableAvatarUrls { get; set; }
+
+        [JsonPropertyName("stable_description")]
+        public object StableDescription { get; set; }
+
+        [JsonPropertyName("stable_name")]
+        public string StableName { get; set; }
+
+        [JsonPropertyName("stable_slug")]
+        public string StableSlug { get; set; }
+
+        [JsonPropertyName("thoroughbreds")]
+        public int Thoroughbreds { get; set; }
+
+        [JsonPropertyName("win_rate")]
+        public double WinRate { get; set; }
+    }
+
+    public class Stable
+    {
+        [JsonPropertyName("stable_stats")]
+        public StableStats StableStats { get; set; }
+    }
+}
